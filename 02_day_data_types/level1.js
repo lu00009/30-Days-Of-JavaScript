c8d9711796e01daf61769a2a6862db623e414d51
lu00009 <lelomohammedpg@gmail.com>
2025-01-23 10:45:44 +0300

    level 1

---
+++ 02_day_data_types/level1.js
@@ -0,0 +1,50 @@
+let variable = ' 30 Days of JavaScript ';
+console.log(variable);
+
+console.log(variable.toUpperCase());
+
+console.log(variable.toLowerCase());
+
+console.log(variable.substring(0,2));
+
+console.log(variable.slice(3));
+
+console.log(variable.includes('Script'));
+
+console.log(variable.split(' '));
+
+let stringed = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+console.log(stringed.split(','));
+
+console.log(variable.replace('JavaScript, Python'));
+
+console.log(variable.charAt(15));
+
+console.log(variable.charCodeAt('J'));
+
+console.log(variable.indexOf('a'));
+
+console.log(variable.lastIndexOf('a'));
+
+let sentence = 'You cannot end a sentence with because because because is a conjunction';
+console.log(sentence.indexOf('because'));
+
+console.log(sentence.lastIndexOf('because'));
+
+console.log(sentence.search('because'));
+
+console.log(variable.trim());
+
+console.log(variable.startsWith(' 30'));
+
+console.log(variable.endsWith('Script '));
+
+console.log(variable.match('a'));
+
+let first = '30 Days of';
+console.log(first.concat(' JavaScript'));
+
+console.log(variable.repeat(2));
+
+
+
