86001f09e4056e56a9ada072e10e9bf6837b0e4c
lu00009 <lelomohammedpg@gmail.com>
2025-01-24 21:46:25 +0300

    level1

---
+++ 03_day_Introduction/level1.js
@@ -0,0 +1,84 @@
+let firstName = 'lelo' 
+let lastName = 'mohamed'
+let country = 'Ethiopia'
+let city = 'Adama'
+let age = 22
+let isMarried = false
+let year= 2025
+console.log(typeof firstName)
+console.log(typeof lastName)
+console.log(typeof country)
+console.log(typeof city)
+console.log(typeof age)
+console.log(typeof isMarried)
+console.log(typeof year)
+
+console.log('10' == 10)
+
+console.log(parseInt('9.8') == 10) 
+
+console.log(10 == 10)
+console.log(!isMarried)
+console.log(10 == 10 && 10 == 10)
+
+console.log(!10 == 10)
+console.log(isMarried)
+console.log(10 === '10' && 10 == 10)
+
+const num1 = 4 > 3 // true
+const num2 = 4 >= 3 // true
+const num3 = 4 < 3 // false
+const num4 = 4 <= 3 // false
+const num5 = 4 == 4 // true
+const num6 = 4 === 4 // true
+const num7 = 4 != 4 // false
+const num8 = 4 !== 4 // false
+const num9 = 4 != '4' // false
+const num10 = 4 == '4' // true
+const num11 = 4 === '4' // false
+console.log(num1)
+console.log(num2)
+console.log(num3)
+console.log(num4)
+console.log(num5)
+console.log(num6)
+console.log(num7)
+console.log(num8)
+console.log(num9)
+console.log(num10)
+console.log(num11)
+console.log('python'.length > 'dragon'.length)
+
+let log1 = 4 > 3 && 10 < 12 // true
+let log2 = 4 > 3 && 10 > 12 // false
+let log3 = 4 > 3 || 10 < 12 // true
+let log4 = 4 > 3 || 10 > 12 // true
+let log5 = !(4 > 3) // false
+let log6 = !(4 < 3) // true
+let log7 = !(false) // true
+let log8 = !(4 > 3 && 10 < 12) // false
+let log9 = !(4 > 3 && 10 > 12) // true
+let log10 = !(4 === '4') // true
+let log11 = 'dragon'.includes('on') && 'python'.includes('on') // true
+
+console.log(log1)
+console.log(log2)
+console.log(log3)
+console.log(log4)
+console.log(log5)
+console.log(log6)
+console.log(log7)
+console.log(log8)
+console.log(log9)
+console.log(log10)
+console.log(log11)
+
+const now = new Date()
+console.log(now.getFullYear())
+console.log(now.getMonth())
+console.log(now.getDate())
+console.log(now.getDay())
+console.log(now.getHours())
+console.log(now.getMinutes())
+console.log(now.getSeconds())
+
