6178e04a2e070dd809dfc7031cf427cf39d39b02
lu00009 <lelomohammedpg@gmail.com>
2025-01-25 19:48:07 +0300

    level3

---
+++ 04_day_conditionals/level3.js
@@ -0,0 +1,42 @@
+//1
+const month = prompt('Enter the month:')
+switch(month){
+  case 'January':
+    console.log('31 days')
+    break
+  case 'February':
+    console.log('28 days in a common year and 29 days in leap years')
+    break
+  case 'March':
+    console.log('31 days')
+    break
+  case 'April':
+    console.log('30 days')
+    break
+  case 'May':
+    console.log('31 days')
+    break
+  case 'June':
+    console.log('30 days')
+    break
+  case 'July':
+    console.log('31 days')
+    break
+  case 'August':
+    console.log('31 days')
+    break
+  case 'September':
+    console.log('30 days')
+    break
+  case 'October':
+    console.log('31 days')
+    break
+  case 'November':
+    console.log('30 days')
+    break
+  case 'December':
+    console.log('31 days')
+    break
+  default:
+    console.log('Not a valid month')
+}
